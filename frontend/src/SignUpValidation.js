b89e821d6bce1f679e03d1f40c2a7bf3e9d9baa1
Arun AK <94607787+ARUN-AK5396@users.noreply.github.com>
2023-10-17 12:48:02 +0530

    Update SignUpValidation.js

--- frontend/src/SignUpValidation.js
+++ frontend/src/SignUpValidation.js
@@ -1,33 +1,30 @@
-function validation(values){
-    let error = {}
+function validation(values) {
+    const error = {};
     const email_pattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
     const password_pattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
 
-    if(values.name === ""){
-        error.name = "Name should not be empty"
-    }
-    else {
-        error.name = ""
-    }
-    if(values.email === ""){
-        error.email = "Email should not be empty"
-    }
-    else if(!email_pattern.test(values.email)){
-    error.email = "Email did not match"
-    }
-    else {
-        error.email = ""
+    if (!values.name) {
+        error.name = "Name should not be empty";
+    } else {
+        error.name = "";
     }
 
-    if(values.password === ""){
-        error.password = "Password should not be empty"
+    if (!values.email) {
+        error.email = "Email should not be empty";
+    } else if (!email_pattern.test(values.email)) {
+        error.email = "Invalid email format";
+    } else {
+        error.email = "";
     }
-    else if (!password_pattern.test(values.password)) {
-        error.password = 'Password must contain at least one uppercase letter, one lowercase letter, a number and a special character'
+
+    if (!values.password) {
+        error.password = "Password should not be empty";
+    } else if (!password_pattern.test(values.password)) {
+        error.password = 'Password must contain at least one uppercase letter, one lowercase letter, a number, and a special character';
+    } else {
+        error.password = "";
     }
-    else{
-        error.password = ""
-    }
+
     return error;
 }
 
